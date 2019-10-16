b5370aa3846e6a072007eef27d90539041bde966
tnicolas <tnicolas@student.42.fr>
2019-10-16 19:22:08 +0200

    update main test or ex03

--- D03/ex03/main.cpp
+++ D03/ex03/main.cpp
@@ -1,33 +1,43 @@
 #include "FragTrap.hpp"
 #include "ScavTrap.hpp"
 #include "NinjaTrap.hpp"
+#include <iostream>
 
 int main(int ac, char **av)
 {
     (void)ac;
     (void)av;
+    std::cout << "// init" << std::endl;
     FragTrap frag("fragPlayer");
-    frag.rangedAttack("target");
-    frag.meleeAttack("target");
-    frag.takedamage(30);
-    frag.beRepaired(5);
-    frag.vaulthunter_dot_exe("target");
-    frag.vaulthunter_dot_exe("target");
-    frag.vaulthunter_dot_exe("target");
-    frag.vaulthunter_dot_exe("target");
-    frag.vaulthunter_dot_exe("target");
-    frag.vaulthunter_dot_exe("target");
     ScavTrap scav("scavPlayer");
-    scav.rangedAttack("target");
-    scav.meleeAttack("target");
-    scav.takedamage(30);
-    scav.beRepaired(5);
-    scav.challengeNewcomer();
-    scav.challengeNewcomer();
-    scav.challengeNewcomer();
-    scav.challengeNewcomer();
     NinjaTrap ninja("ninjaPlayer");
+    std::cout << "// attack" << std::endl;
+    frag.rangedAttack("target");
+    scav.rangedAttack("target");
+    ninja.rangedAttack("target");
+    frag.meleeAttack("target");
+    scav.meleeAttack("target");
+    ninja.meleeAttack("target");
+    frag.takedamage(30);
+    scav.takedamage(30);
+    frag.beRepaired(5);
+    scav.beRepaired(5);
+    frag.vaulthunter_dot_exe("target");
+    scav.challengeNewcomer();
+    frag.vaulthunter_dot_exe("target");
+    scav.challengeNewcomer();
+    frag.vaulthunter_dot_exe("target");
+    scav.challengeNewcomer();
+    frag.vaulthunter_dot_exe("target");
+    scav.challengeNewcomer();
+    frag.vaulthunter_dot_exe("target");
+    scav.challengeNewcomer();
+    frag.vaulthunter_dot_exe("target");
+    scav.challengeNewcomer();
+    std::cout << "// ninja attack frag" << std::endl;
     ninja.ninjaShoebox(frag);
+    std::cout << "// ninja attack scav" << std::endl;
     ninja.ninjaShoebox(scav);
+    std::cout << "// delete" << std::endl;
     return 0;
 }
