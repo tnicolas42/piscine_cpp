c5aac37224c98f61d2ba68c7e0951ad5181573af
tnicolas <tnicolas@student.42.fr>
2019-10-24 13:56:31 +0200

    add ^D to send 0

--- D08/ex03/GetInput.cpp
+++ D08/ex03/GetInput.cpp
@@ -22,8 +22,16 @@ GetInput &GetInput::operator=(GetInput const &rhs) {
 
 bool GetInput::exec() {
     int ci = std::getchar();
-    if ((ci >= 32 && ci <= 126) || ci == '\n' || ci == '\t') {
+    if ((ci >= 32 && ci <= 126)) {
         _brfk->getData()[_brfk->getPtr()] = static_cast<char>(ci);
+        return true;
+    }
+    if (ci == -1) {  // ^D -> save 0
+        _brfk->getData()[_brfk->getPtr()] = 0;
+        return true;
+    }
+    if (ci == '\n') {  // \n -> retry instruction
+        _brfk->getProgPtr()--;
         return true;
     }
     return false;
